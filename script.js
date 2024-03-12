8f548de168c3c70151366e9e6106aab1b160b12f
rasheeda <raskieda@gmail.com>
2024-03-12 16:38:06 +0200

    added comments

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (/^pet_(?=.*[0-9]{4})(?=.*[a-zA-Z])[a-zA-Z\d]+$/.test(input)) { // ^pet is included in start of string, ?=.*[0-9] = ensures at least one number is included, [0-9]{4} ensures a 4-digit number which is correct for year, (?=.*[a-zA-Z]) ensures at least one character upper or lowercase, [a-zA-Z\d]+ ensures a match of alphanumeric characters 
-            result = 'Valid Syntax';
+        if (/^pet_(?=.*[0-9]{4})(?=.*[a-zA-Z])[a-zA-Z\d]+$/.test(input)) { // ^ starts string and 'pet_' is included in start of string, ?=.*[0-9] = ensures at least one number is included, [0-9]{4} ensures a 4-digit number which is correct for year, (?=.*[a-zA-Z]) ensures at least one character upper or lowercase, [a-zA-Z\d]+ ensures there is a match of alphanumeric characters, $ ends string. 
+            result = 'Valid Syntax';                                        // test() regular expression (RegEx) method
         } else {
             result = 'Invalid Syntax';
         }
-            document.getElementById('result').innerText = result;
+            document.getElementById('result').innerText = result;  // displays the result message as valid or invalid based on input
 }
 
