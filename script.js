21867f1dd6a8b0d2dd68b77d5d7d942e32e2ae6d
rasheeda <raskieda@gmail.com>
2024-03-12 11:43:55 +0200

    comments added to if statement

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (/^pet_(?=.*[0-9]{4})(?=.*[a-zA-Z])[a-zA-Z\d]+$/.test(input)) { 
+        if (/^pet_(?=.*[0-9]{4})(?=.*[a-zA-Z])[a-zA-Z\d]+$/.test(input)) { // ^pet is included in start of string, ?=.*[0-9] = ensures at least one number is included, [0-9]{4} ensures a 4-digit number which is correct for year, (?=.*[a-zA-Z]) ensures at least one character upper or lowercase, [a-zA-Z\d]+ ensures a match of alphanumeric characters 
             result = 'Valid Syntax';
         } else {
             result = 'Invalid Syntax';
