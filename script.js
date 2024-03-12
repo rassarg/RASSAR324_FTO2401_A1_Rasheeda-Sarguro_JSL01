f990e794d32a8886a74a80b192702c3e87fdacb9
rasheeda <raskieda@gmail.com>
2024-03-12 11:35:41 +0200

    completed if else statement

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
-        if (/^pet_(?=.*[0-9]{4})
+        if (/^pet_(?=.*[0-9]{4})(?=.*[a-zA-Z])[a-zA-Z\d]+$/.test(input)) { 
+            result = 'Valid Syntax';
+        } else {
+            result = 'Invalid Syntax';
         }
             document.getElementById('result').innerText = result;
 }
